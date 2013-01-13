ae844120cce5ff374221cfb1c438a4997a973c04
Cam Swords <cam.swords@gmail.com>
2013-01-14 10:03:29 +1100

    made the helloworld and hello_world names consistent

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -2,9 +2,9 @@ class truth::enforcer {
 
 	include java
 
-	if has_role("hello_world") {
-		class { 'java_program': jar_file => "helloworld-1.0-standalone.jar", program_name => "hello_world" }	
+	if has_role("helloworld") {
+		class { 'java_program': jar_file => "helloworld-1.0-standalone.jar", program_name => "helloworld" }	
 	} else {
-		class { 'java_program::remove': program_name => "hello_world" }
+		class { 'java_program::remove': program_name => "helloworld" }
 	}
 }
