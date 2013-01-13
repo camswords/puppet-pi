03354cdb4a808cb7c49a419312e3b09a01f3c42a
Cam Swords <cam.swords@gmail.com>
2013-01-13 18:19:01 +1100

    Added a the nodeless configuration.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,3 @@
-import "templates"
-import "nodes"
-
+node default {
+	include truth::enforcer
+}
---
+++ modules/truth/manifests/enforcer.pp
@@ -0,0 +1,9 @@
+class truth::enforcer {
+
+	if has_role("led_fun_and_games") {
+		notice("I play with leds, they're fun!")
+	} else {
+		notice("no leds for me!")
+	}
+
+}
