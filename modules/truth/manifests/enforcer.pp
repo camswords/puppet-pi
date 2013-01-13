6798879e5a7cdf7cc883bfa2d84ec3bd6668ef72
Cam Swords <cam.swords@gmail.com>
2013-01-13 19:00:05 +1100

    reverted the function name to print in the enforcer

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -1,11 +1,11 @@
 class truth::enforcer {
 
 	if has_role("led_fun_and_games") {
-		notify("I play with leds, they're fun!")
+		notice("I play with leds, they're fun!")
 	} else {
-		notify("no leds for me!")
+		notice("no leds for me!")
 	}
 	
-	notify("ummm... does this work?")
+	notice("ummm... does this work?")
 
 }
