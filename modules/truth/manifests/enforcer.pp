f736828e58721f30dc42e20bf4de6f0836255263
Cam Swords <cam.swords@gmail.com>
2013-01-14 10:02:21 +1100

    removed the dollar signs, put them in the wrong place!

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -3,8 +3,8 @@ class truth::enforcer {
 	include java
 
 	if has_role("hello_world") {
-		class { 'java_program': $jar_file => "helloworld-1.0-standalone.jar", $program_name => "hello_world" }	
+		class { 'java_program': jar_file => "helloworld-1.0-standalone.jar", program_name => "hello_world" }	
 	} else {
-		class { 'java_program::remove': $program_name => "hello_world" }
+		class { 'java_program::remove': program_name => "hello_world" }
 	}
 }
