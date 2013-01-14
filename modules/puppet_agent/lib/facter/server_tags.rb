b99f71484baf65863c6af3cb8295c30be3f75462
Cam Swords <cam.swords@gmail.com>
2013-01-14 18:41:57 +1100

    added the setcode to the facter class, maybe this is why its not working!

--- modules/puppet_agent/lib/facter/server_tags.rb
+++ modules/puppet_agent/lib/facter/server_tags.rb
@@ -1,5 +1,7 @@
 require 'facter'
 
 Facter.add(:server_tags) do
-  Facter::Util::Resolution.exec("cat /etc/puppet/server_tags 2>/dev/null")
+  setcode do
+    Facter::Util::Resolution.exec("cat /etc/puppet/server_tags 2>/dev/null")
+  end
 end
