0c3136b4e7a72a6b20fbbbf25a3eb91cbc88122c
Cam Swords <cam.swords@gmail.com>
2013-01-15 20:14:06 +1100

    reverted the server_tags to be present now that I am sure that the role function handles lack of that file appropriately

--- modules/puppet_agent/manifests/init.pp
+++ modules/puppet_agent/manifests/init.pp
@@ -5,8 +5,10 @@ class puppet_agent {
 		ensure => present,
 		source => "puppet:///modules/puppet_agent/etc/puppet/puppet.conf"
 	}
-	
+
 	file { "/etc/puppet/server_tags":
-		ensure => absent
-	}	
+		ensure => present,
+		source => "puppet:///modules/puppet_agent/etc/puppet/server_tags",
+		mode => 0644
+	}
 }
