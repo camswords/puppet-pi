d15ec4e93409ee48c4cb75c714c87d9519f0712b
Cam Swords <cam.swords@gmail.com>
2013-01-15 19:51:20 +1100

    selling my soul and adding some debug statements. lets see what happens.

--- modules/puppet_agent/lib/facter/server_tags.rb
+++ modules/puppet_agent/lib/facter/server_tags.rb
@@ -1,6 +1,12 @@
 
+File.open('/tmp/camtesta', 'w') { |file| file.write("the code before the facter add was called." )}
+
 Facter.add(:kernell) do
+
+  File.open('/tmp/camtestb', 'w') { |file| file.write("the code in the facter add and before setcode was called." )}
+
   setcode do
+    File.open('/tmp/camtestc', 'w') { |file| file.write("the code in setcode was called." )}
     require 'facter/util/config'
 
     if Facter::Util::Config.is_windows?
