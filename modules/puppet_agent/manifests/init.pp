8682887adc446ecad09bd67a8242e8548feefb30
Cam Swords <cam.swords@gmail.com>
2013-01-15 20:09:41 +1100

    remove the server tags role to see how the role function copes.

--- modules/puppet_agent/manifests/init.pp
+++ modules/puppet_agent/manifests/init.pp
@@ -7,9 +7,6 @@ class puppet_agent {
 	}
 	
 	file { "/etc/puppet/server_tags":
-		ensure => present,
-		source => "puppet:///modules/puppet_agent/etc/puppet/server_tags",
-		mode => 0644
-	}
-	
+		ensure => absent
+	}	
 }
