df454b217b976e46cc899e75023e5d063d8aa11e
Cam Swords <cam.swords@gmail.com>
2013-02-05 21:42:25 +1100

    added the java binary to source control. its too much of a pain to download.

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -1,7 +1,22 @@
 class java {
 
-	package { "openjdk-6-jre-headless":
-		ensure => installed
+	file { "java-install-file":
+		path   => "/opt/ejre-7u10-fcs-b18-linux-arm-vfp-client_headless-28_nov_2012.gz",
+		ensure => present,
+		source => "puppet:///modules/java/tmp/ejre-7u10-fcs-b18-linux-arm-vfp-client_headless-28_nov_2012.gz"
 	}
-
+	
+	exec { "untar-install-file":
+		command => "tar -xf ejre-7u10-fcs-b18-linux-arm-vfp-client_headless-28_nov_2012.gz",
+		cwd     => "/opt",
+		creates => "/opt/ejre1.7.0_10",
+		path    => ["/bin"],
+		require => File["java-install-file"]
+	}
+	
+	file { "create-java-symlink":
+		path    => "/usr/bin/java",
+		ensure  => link,
+		target  => "/opt/ejre1.7.0_10/bin/java"
+	}
 }
