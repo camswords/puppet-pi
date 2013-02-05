36809176e1f905147c5899f4ad46dcf03295896d
Cam Swords <cam.swords@gmail.com>
2013-02-05 22:19:01 +1100

    the puppet agent module should always be installed

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -3,14 +3,11 @@ class truth::enforcer {
 	notice("$fqdn, I see that you're a: $server_tags")
 
 	include java
+	include puppet_agent
 
 	if has_role("helloworld") {
 		class { 'java_program': jar_file => "helloworld-1.0-standalone.jar", program_name => "helloworld" }	
 	} else {
 		class { 'java_program::remove': program_name => "helloworld" }
-	}
-	
-	if has_role("puppet_agent") {
-		include puppet_agent
 	}
 }
